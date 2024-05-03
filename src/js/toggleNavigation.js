167e30c5de8a95b3c3545446745d5fb9439ca884
Liuba <1990yavorska@gmail.com>
2024-05-03 17:09:52 +0200

    Changed variables names in 'toggleNavigation.js'

--- src/js/toggleNavigation.js
+++ src/js/toggleNavigation.js
@@ -1,22 +1,22 @@
 const onDomContentLoaded = () => {
-  const $burgerElem = document.querySelector('#burger');
-  const $navElem = document.querySelector('.header__nav');
+  const burgerElem = document.querySelector('#burger');
+  const navElem = document.querySelector('.header__nav');
 
   function toggleNav() {
     this.classList.toggle('is-active');
-    $navElem.classList.toggle('is-open');
+    navElem.classList.toggle('is-open');
   }
 
   function checkActiveClass() {
-    const display = getComputedStyle($burgerElem).getPropertyValue('display');
+    const display = getComputedStyle(burgerElem).getPropertyValue('display');
 
     if (display === 'none') {
-      $burgerElem.classList.remove('is-active');
-      $navElem.classList.remove('is-open');
+      burgerElem.classList.remove('is-active');
+      navElem.classList.remove('is-open');
     }
   }
 
-  $burgerElem.addEventListener('click', toggleNav);
+  burgerElem.addEventListener('click', toggleNav);
   window.addEventListener('resize', checkActiveClass);
 };
 
